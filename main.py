bfb5ea9a2df5fa47848d83799c68458fe35aa1b8
larynovaes <>
2022-02-13 20:50:34 +0000

    Criar variariaveis para calcular aumento de salario

--- main.py
+++ main.py
@@ -22,3 +22,6 @@ print('Valor com desconto: R$', valor * 0.8)
 # 3. Escreva um programa que recebe o valor de um salário e um percentual de aumento e imprime o valor do aumento e o novo salário.
 
 salario = float( input("Salario sem percentual de aumento: ") )
+
+aumento = salario * 0.2
+novoSalario = salario + aumento
