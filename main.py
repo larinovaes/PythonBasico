f9ba5858283af9ed8547ac5e4c3d64ea8543712f
larynovaes <>
2022-02-13 20:47:28 +0000

    Printar valores com calculos para somar desconto de produto

--- main.py
+++ main.py
@@ -14,3 +14,7 @@ print('Quantidade de segundos: ', segundos)
 # 2. Faça um programa que recebe o valor de um produto e um percentual de desconto e imprime a valor do desconto e o preço a pagar.
 
 valor = float( input("Valor original: R$ ") )
+
+print('Valor sem desconto:     R$', valor)
+print('Desconto ganho:     R$', valor * 0.2)
+print('Valor com desconto: R$', valor * 0.8)
